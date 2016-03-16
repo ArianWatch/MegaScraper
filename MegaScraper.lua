102821fa913ff793a03f6e758b7bccac459ce607
DawshArian <DAwshArian@gmail.com>
2016-03-16 16:47:17 +0330

    Update and rename bot/nod32bot.lua to MegaScraper.lua

--- MegaScraper.lua
+++ MegaScraper.lua
@@ -245,14 +245,14 @@ function create_config( )
     "time",
     "version"
     },
-	    sudo_users = {158990680},--Sudo users
+	    sudo_users = {137984481},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
 https://github.com/BH-YAGHI/NOD32-BOT.git
 
-channel : @Nod32team
-sodu : @behrooZyaghi
+
+sodu : @DawshArian
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -317,11 +317,10 @@ Only sudo users can run this command
 !bc 123456789 Hello !
 This command will send text to [group_id]
 
-ch: @Nod32team
 
 ]],
     help_text = [[
-NOD32 Commands list :
+Mega Scraper Commands
  
 1-banhammer list ^
  
@@ -516,9 +515,9 @@ NOD32 Commands list :
 ارسال نظرات به سازنده
 
 you can use both "/" and "!" 
-.شما میتوانید از ! و / استفاده کنید
+.شما میتوانید از ! و / و # استفاده کنید
 
-channel: @Nod32Team
+
 
 G00D LUCK ^_^
 ]]
